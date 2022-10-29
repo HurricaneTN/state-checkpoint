f040ce2690f16f0177bca6ca8200902039d0b6f6
Hurricane.TN <houssem.rouabeh@toucomex.com>
2022-10-29 17:40:54 +0100

    third commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,79 @@
-import { Component } from "react";
 import "./App.css";
-import Test from "./Test";
-import MyImage from "../src/me.jpg";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import MyImage from "./profile/me.jpg";
 
-export default class App extends Component {
-  state = {
-    fullName: "Houssem Rouabeh",
-    bio: "Je suis Houssem Rouabeh la plupart d’entre vous me connaissent, évidement un étudiant comme vous tous, mais aussi pour celui qui ne me connait pas je suis un professionnel dans le domaine de l’IT.",
-    imgSrc: { MyImage },
-    profession: "IT Manager",
-    show: true,
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: 0,
+      show: false,
+      x: "Show",
+      Person: {
+        fullName: "Houssem Rouabeh",
+        bio: "GoMyCode student",
+        imgSrc: { MyImage },
+        profession: "IT Manager",
+      },
+    };
+  }
+
+  handleClick = () => {
+    this.setState({ show: !this.state.show, time: 0 });
+    if (this.state.show === false) {
+      this.setState({ show: true });
+      this.setState({ x: "Hide" });
+    } else if (this.state.show === true) {
+      this.setState({ show: false });
+      this.setState({ x: "Show" });
+    }
   };
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        time: this.state.time + 1,
+      });
+    }, 1000);
+  }
   render() {
+    const { fullName, bio, imgSrc, profession } = this.state.Person;
+
     return (
-      <div>
-        <img src={MyImage} alt="moi" />
-        {console.log(this.state.imgSrc)}
+      <div className="all">
+        <div className={this.state.show ? "visible" : "hide"}>
+          <div className="container-fluid">
+            <div class="row">
+              <div class="col-md-12">
+                <div class="row">
+                  <div class="col-md-6">
+                    <img src={imgSrc} alt="me" />
+                  </div>
+                  <div class="col-md-6">
+                    <p> Hi ! my name is {fullName}</p>
+
+                    <p> {bio}</p>
+                    <p>
+                      I'm <span>{profession}</span>
+                    </p>
+                    <h2>
+                      <span class="noselect">{this.state.time}</span>
+                      <div id="circle"></div>
+                    </h2>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="visible">
+          <div id="btn" onClick={this.handleClick}>
+            <span class="noselect">{this.state.x}</span>
+            <div id="circle"></div>
+          </div>
+        </div>
       </div>
     );
   }
